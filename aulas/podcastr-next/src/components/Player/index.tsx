cdcb09ed224f0663a3f8afc1a82a7b832e694104
DanielOliveiraSouza <oliveira.daniel109@gmail.com>
2021-04-24 03:24:56 -0300

    Finaliza componente Player

--- aulas/podcastr-next/src/components/Player/index.tsx
+++ aulas/podcastr-next/src/components/Player/index.tsx
@@ -12,12 +12,15 @@ export default function Player (){
                 <strong>Selecione um podcast para ouvir</strong>
             </div>
             
-            <footer>
+            <footer className={styles.empty}>
 
                 <div className={styles.progress}>
                     <span>00:00</span>
-                    <div className={styles.emptySlider}/>
+                    <div className={styles.slider}>
+                        <div className={styles.emptySlider}/>
+                    </div>
                     <span>00:00</span>
+
                 </div>
                 <div className={styles.buttons}>
                     <button type="button">
@@ -26,7 +29,7 @@ export default function Player (){
                     <button type="button">
                         <img src="/play-previous.svg" alt="Tocar anterior"/>
                     </button>
-                    <button type="button" className="styles.playerButton">
+                    <button type="button" className={styles.playButton}>
                         <img src="/play.svg" alt="Tocar"/>
                     </button>
 
