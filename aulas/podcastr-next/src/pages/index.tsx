96889bfdeef45f7fe8d270660ed399f6c29b0757
DanielOliveiraSouza <oliveira.daniel109@gmail.com>
2021-04-25 23:46:32 -0300

    adiciona suporte a geraçao de paginas estaticas em json

--- aulas/podcastr-next/src/pages/index.tsx
+++ aulas/podcastr-next/src/pages/index.tsx
@@ -1,7 +1,24 @@
-import Header from "../components/Header";
+import { useEffect } from "react"
 
-export default function Home() {
+interface HomeProps {
+  episodes: JSON
+}
+//SSG
+export default function Home(props:HomeProps) {
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export  async function getStaticProps(){
+    const response = await  fetch('http://localhost:3333/episodes')
+    const data  = await response.json();
+
+    return {
+      props:{episodes:data},
+      revalidate: 60 * 60 * 8,
+    }
 }
