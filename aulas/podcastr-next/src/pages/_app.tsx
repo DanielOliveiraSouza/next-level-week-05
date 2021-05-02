b7211bcbdcb44edf783a06ddeae95f20fa8a804a
DanielOliveiraSouza <oliveira.daniel109@gmail.com>
2021-05-02 18:17:06 -0300

    adiciona funcionalide de tocar a playList

--- aulas/podcastr-next/src/pages/_app.tsx
+++ aulas/podcastr-next/src/pages/_app.tsx
@@ -3,30 +3,12 @@ import '../styles/global.scss'
 
 import styles from '../styles/app.module.scss';
 import Player from '../components/Player';
-import { PlayerContext } from '../components/contexts/PlayerContext';
+import { PlayerContext, PlayerContextProvider } from '../components/contexts/PlayerContext';
 import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
-
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex ] = useState(0);
-  const [isPlaying,setIsPlaying] = useState(false);
-
-  function play(episode){
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-  function togglePlay (){
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state: boolean) {
-    setIsPlaying(state);
-  }
-
-  return (
-    <PlayerContext.Provider value={{episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState}} > 
+  return (  
+    <PlayerContextProvider>
       <div className={styles.wrapper}>
         
         <main>
@@ -37,7 +19,7 @@ function MyApp({ Component, pageProps }) {
         <Player/>
         
     </div>
-    </PlayerContext.Provider>
+    </PlayerContextProvider>
   )
 
 }
--- aulas/podcastr-next/src/pages/index.tsx
+++ aulas/podcastr-next/src/pages/index.tsx
@@ -33,13 +33,14 @@ type HomeProps = {
 //SSG
 export default function Home({latestEpisodes,allEpisodes}:HomeProps) {
   
-  const {play }  = useContext(PlayerContext);
+  const {playList} = useContext(PlayerContext);
+  const episodeList = [...latestEpisodes,...allEpisodes]
   return  (
     <div className={styles.homepage}>
     <section className={styles.latestEpisodes}>
       <h2>Últimos lançamentos</h2>
       <ul>
-        {latestEpisodes.map(episode =>{
+        {latestEpisodes.map((episode,index) =>{
           return (
             <li key={episode.id}>
               <Image 
@@ -60,7 +61,7 @@ export default function Home({latestEpisodes,allEpisodes}:HomeProps) {
                 <span>{episode.durationAsString}</span>
                 </div>
                 
-                <button type="button" onClick={()=>{ play(episode)}}>
+                <button type="button" onClick={()=>{ playList(episodeList,index)}}>
 
                   <img src="/play-green.svg" alt="Tocar Episódio"/>
                 </button> 
@@ -83,7 +84,7 @@ export default function Home({latestEpisodes,allEpisodes}:HomeProps) {
             </tr>
           </thead>
           <tbody>
-            {allEpisodes.map(episode => {
+            {allEpisodes.map((episode, index) => {
               return (
               <tr key={episode.id}>  
                 <td>
@@ -107,7 +108,7 @@ export default function Home({latestEpisodes,allEpisodes}:HomeProps) {
                   </td>
                 <td>{episode.durationAsString}</td>
                 <td>
-                  <button type="button" onClick={()=>play(episode)}>
+                  <button type="button" onClick={ ()=> playList(episodeList,index+latestEpisodes.length)}>
                     <img src="/play-green.svg" alt="Tocar podcast"/>
                   </button>
                 </td>
